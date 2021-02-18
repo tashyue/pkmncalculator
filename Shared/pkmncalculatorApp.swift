146562270915467fd19e8e08df9df00caff6070e
Tyler Ashton Yue <tashyue@gmail.com>
2021-02-17 20:45:56 -0800

    Initial Commit

---
+++ Shared/pkmncalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  pkmncalculatorApp.swift
+//  Shared
+//
+//  Created by Tyler Ashton Yue on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct pkmncalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
